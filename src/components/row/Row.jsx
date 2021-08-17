b10670ea08bf53565faf8e7d9f4e6962f8a854d1
Oliver <oliver.dentistry@gmail.com>
2021-08-17 19:52:31 +0200

    api integration

--- src/components/row/Row.jsx
+++ src/components/row/Row.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import "./row.css"
+import React, { useEffect, useState } from "react";
+import axios from "../../axios";
+import { v4 as uuid } from "uuid";
+import "./row.css";
 
-function Row({title, fetchUrl, isLargeRow }) {
-    return (
-        <div className="row">
-            <h1 className="row__title">{title}</h1>
-        </div>
-    )
+function Row({ title, fetchUrl, isLargeRow = false }) {
+  const [movies, setMovies] = useState([]);
+  const baseUrl = "https://image.tmdb.org/t/p/original/";
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+
+    fetchData();
+  }, [fetchUrl]);
+  console.log(isLargeRow);
+  return (
+    <div className="row" key={uuid}>
+      <h2>{title}</h2>
+      <div className="row__posters" key={uuid()}>
+        {movies.map(
+          (movie) =>
+            ((isLargeRow && movie.poster_path) || (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                key={uuid()}
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                src={`${baseUrl}${
+                  movie.poster_path
+                }`}
+                alt={movie.name}
+              />
+            )
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Row
+export default Row;
