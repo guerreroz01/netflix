6b90ba085421725b2370dcef0159668d9adfbd52
Oliver <oliver.dentistry@gmail.com>
2021-08-17 20:54:21 +0200

    scroll draw behavior implemented in desktop browsers

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,33 +1,32 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "../../axios";
 import requests from "../../Request";
 import "./banner.css";
 
 export default function Banner() {
+  const [movie, setMovie] = useState([]);
 
-    const [movie, setMovie] = useState([])
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
 
-    useEffect(()=> {
-        async function fetchData() {
-            const request = await axios.get(requests.fetchNetflixOriginals)
-            setMovie(
-                request.data.results[
-                    Math.floor(Math.random() * request.data.results.length -1)
-                ]
-            )
-            return request
-        }
+    fetchData();
+  }, []);
 
-        fetchData()
-    }, [])
-
-    console.log(movie)
+  console.log(movie);
 
   const truncate = (string, n) => {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   };
 
-  return ( 
+  return (
     <header
       className="banner"
       style={{
@@ -37,21 +36,19 @@ export default function Banner() {
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">{movie?.name || movie?.original_name || movie?.title}</h1>
+        <h1 className="banner__title">
+          {movie?.name || movie?.original_name || movie?.title}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
-          {truncate(
-            movie?.overview,
-            250
-          )}
+          {truncate(movie?.overview, 250)}
         </h1>
       </div>
       <div className="banner__fadeBottom"></div>
     </header>
-     
   );
 }
 
